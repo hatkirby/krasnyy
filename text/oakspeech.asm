04662d55eda49b7540be60fef9761ec64d262363
Kelly Rauchenberger <fefferburbia@gmail.com>
2017-06-10 10:51:10 -0400

    Translated Oak's speech
    Перевела речь Оака.

--- text/oakspeech.asm
+++ text/oakspeech.asm
@@ -1,55 +1,55 @@
 _OakSpeechText1::
-	text "Hello there!"
-	line "Welcome to the"
-	cont "world of #MON!"
+	text "Здравствуйте!"      ; Greetings!
+	line "Добро пожаловать"   ; Welcome
+	cont "в мире ПОКЕМОНа!"   ; to the world of Pokemon!
 
-	para "My name is OAK!"
-	line "People call me"
-	cont "the #MON PROF!"
+	para "Меня зовут ОАК!"    ; My name is OAK!
+	line "Люди зовут меня"    ; People call me
+	cont "Профессор"          ; the Professor
+	cont "ПОКЕМОНа!"          ; of POKEMON!
 	prompt
 
 _OakSpeechText2A::
-	text "This world is"
-	line "inhabited by"
-	cont "creatures called"
-	cont "#MON!@@"
+	text "Этот мир заселён"   ; This world is inhabited
+	line "существами,"        ; by creatures
+	cont "которые называют"   ; which are called
+	cont "ПОКЕМОНов!@@"       ; POKEMON!
 
 _OakSpeechText2B::
-	text $51,"For some people,"
-	line "#MON are"
-	cont "pets. Others use"
-	cont "them for fights."
+	text $51,"Некоторым,"     ; To some people,
+	line "ПОКЕМОНы домашние"  ; POKEMON are
+	cont "животные. Другие"   ; pets. Others
+	cont "ими бьются."        ; use them to fight.
 
-	para "Myself..."
+	para "Я..."               ; I...
 
-	para "I study #MON"
-	line "as a profession."
+	para "Я их изучаю."       ; I study them.
 	prompt
 
 _IntroducePlayerText::
-	text "First, what is"
-	line "your name?"
+	text "Во-первых, как"     ; First, what
+	line "тебя зовут?"        ; is your name?
 	prompt
 
 _IntroduceRivalText::
-	text "This is my grand-"
-	line "son. He's been"
-	cont "your rival since"
-	cont "you were a baby."
+	text "Это мой внук. Он"   ; This is my grandson. He
+	line "твой соперник с"    ; has been your rival
+	cont "тех пор, как твой"  ; since you
+	cont "был младенец."      ; were a baby.
 
-	para "...Erm, what is"
-	line "his name again?"
+	para "...Эээ, как его"    ; ...Erm, what is his
+	line "зовут?"             ; name?
 	prompt
 
 _OakSpeechText3::
 	text $52,"!"
 
-	para "Your very own"
-	line "#MON legend is"
-	cont "about to unfold!"
+	para "Твоё самое своё"    ; Your very own
+	line "сказание ПОКЕМОНа"  ; POKEMON legend
+	cont "готов начать!"      ; is about to start!
 
-	para "A world of dreams"
-	line "and adventures"
-	cont "with #MON"
-	cont "awaits! Let's go!"
+	para "Мир мечтаний и"     ; A world of dreams and
+	line "приключений с"      ; adventures with
+	cont "ПОКЕМОНами ждет!"   ; POKEMON awaits!
+	cont "Идём!"              ; Let's go!
 	done
